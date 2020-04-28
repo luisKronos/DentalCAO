1f3f37e6b1548cbb55195213630e9a6f1b9578eb
luisKronos <luisdiaz961013@gmail.com>
2020-04-28 10:06:01 -0500

    Actualizado hasta tratamiento - detalles

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 75
+    offset: 155
   });
 
   // Collapse Navbar
